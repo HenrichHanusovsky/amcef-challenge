4829dc1f03cb6c15262f7eb0cbdd927ce9494016
Henrich Hanusovsky <henrich.hanusovsky@remastr.com>
2023-04-04 12:08:25 +0200

    Initial commit

---
+++ src/main/java/com/hanusovsky/amcef/controller/PostController.java
@@ -0,0 +1,16 @@
+package com.hanusovsky.amcef.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/post")
+public class PostController {
+
+    @GetMapping
+    public String getPosts() {
+        return "Yes";
+    }
+}
---
+++ src/test/java/com/hanusovsky/amcef/AmcefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanusovsky.amcef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmcefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
